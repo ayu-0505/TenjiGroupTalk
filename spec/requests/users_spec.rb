4029e426b6963f35aa128609b50379fdcae86fd9
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-07-02 14:57:02 +0900

    rubocopの指摘の修正

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -13,7 +13,6 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/users", type: :request do
-  
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
@@ -74,7 +73,7 @@ RSpec.describe "/users", type: :request do
       it "does not create a new User" do
         expect {
           post users_url, params: { user: invalid_attributes }
-        }.to change(User, :count).by(0)
+        }.not_to change(User, :count)
       end
 
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
