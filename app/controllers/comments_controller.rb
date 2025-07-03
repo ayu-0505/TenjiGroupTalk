2bea04e1857918176ec0a4542bd4fcce2c9ae89b
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-07-03 10:29:12 +0900

    リソース設計に沿うようにルーティング、コントローラー、viewsを変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
 
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
   # POST /comments or /comments.json
   def create
     @comment = Comment.new(comment_params)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :talks
+  has_many :comments
 end
