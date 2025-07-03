98fff06fc88bf6c04b1509a8202f1fa244f00758
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-07-03 14:17:59 +0900

    基本の関連付けを行なった

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,4 +2,5 @@ class Talk < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :comments
+  has_many :subscriptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :talks
   has_many :comments
+  has_many :subscriptions
 end
