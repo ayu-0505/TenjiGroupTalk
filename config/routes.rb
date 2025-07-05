097cb4af7fc8f33a9633d4dc6dcc3722a66631c5
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-07-05 15:23:55 +0900

    ルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
+
+  resources :sessions, only: %i[create destroy]
+
   resources :users, only: %i[show edit update destroy]
   resources :groups do
     resources :talks do
