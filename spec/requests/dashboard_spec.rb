4f5f87d59826839b86962154008b0f71e3350ad9
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-07-07 17:14:32 +0900

    ダッシュボードへの接続テストを追加

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboards", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /dashboard" do
+    it 'display contents only login user' do
+      user = create(:user)
+      log_in_as(user)
+
+      expect(page).to have_text('ログイン後ルート画面')
+    end
   end
 end
