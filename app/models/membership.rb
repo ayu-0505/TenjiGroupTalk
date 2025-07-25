9c71228c0c9de66d5c7bfbf9be6b4add477b9fc0
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-07-25 15:30:49 +0900

    ２重登録禁止のバリデーションを追加

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  # TODO: 招待制のため、招待リンクの機能を作成してからこのバリデーションに関するテストを作成すること
+  validates :user_id, presence: true, uniqueness: { scope: :group_id, message: '同じグループに２重でメンバーになることはできません。' }
 end
