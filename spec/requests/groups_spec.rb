e83d252787b74702dc6d71fbe7e1fa4939f552ab
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-08-01 11:22:21 +0900

    groupのrequest testを整理、テストlogを出力するように変更

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -3,15 +3,123 @@ require 'rails_helper'
 RSpec.describe "/groups", type: :request do
   let(:group) { create(:group) }
   let(:user) { create(:user) }
-  let(:user2) { create(:user) }
   let(:non_member_user) { create(:user) }
 
   before do
     create(:membership, user: user, group: group)
-    create(:membership, user: user2, group: group)
   end
 
-  describe 'PATCH /groups/:id, #update' do
+  describe "GET /index" do
+    before do
+      sign_in user
+    end
+
+    it "renders a successful response" do
+      get groups_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    context 'when member user accesses' do
+      before do
+        sign_in user
+      end
+
+      it "renders a successful response" do
+        get group_url(group)
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when a non-member user accesses' do
+      before do
+        sign_in(non_member_user)
+        get dashboard_path
+      end
+
+      it "redirects to the last requested path (dashboard_path)" do
+        get group_url(group)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+  end
+
+  describe "GET /new" do
+    before do
+      sign_in user
+    end
+
+    it "renders a successful response" do
+      get new_group_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /edit" do
+    context 'when member user accesses' do
+      before do
+        sign_in user
+      end
+
+      it "renders a successful response" do
+        get edit_group_url(group)
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when a non-member user accesses' do
+      before do
+        sign_in(non_member_user)
+        get dashboard_path
+      end
+
+      it "redirects to the last requested path (dashboard_path)" do
+        get edit_group_url(group)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+  end
+
+  describe "POST /create" do
+    before do
+      sign_in user
+    end
+
+    context "with valid parameters" do
+      let(:valid_attributes) { attributes_for(:group, name: 'Valid Group Name') }
+
+      it "creates a new Group" do
+        expect {
+          post groups_url, params: { group: valid_attributes }
+        }.to change(Group, :count).by(1)
+      end
+
+      it "redirects to the created group" do
+        post groups_url, params: { group: valid_attributes }
+        expect(response).to redirect_to(group_url(Group.last))
+      end
+    end
+
+    context "with invalid parameters" do
+        let(:invalid_attributes) { attributes_for(:group, name: "#{'a' * 50} Long Name group") }
+
+      it "does not create a new Group" do
+        expect {
+          post groups_url, params: { group: invalid_attributes }
+        }.not_to change(Group, :count)
+      end
+
+      it "renders a response with 422 status (i.e. to display the 'new' template)" do
+        post groups_url, params: { group: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'PATCH /groups/:id' do
     let(:new_attributes) { attributes_for(:group, name: 'New Group Name') }
 
     context 'when member user accesses' do
@@ -33,7 +141,7 @@ RSpec.describe "/groups", type: :request do
         get dashboard_path
       end
 
-      it 'does not update the group and redirects to root_path' do
+      it 'does not update the group and redirects to the last requested path (dashboard_path)' do
         expect do
           patch group_url(group), params: { group: new_attributes }
         end.not_to change { group.reload.name }
@@ -44,7 +152,7 @@ RSpec.describe "/groups", type: :request do
     end
   end
 
-  describe 'DELETE /groups/:id, #destroy' do
+  describe 'DELETE /groups/:id' do
     context 'when member user accesses' do
       before do
         sign_in(user)
@@ -65,7 +173,7 @@ RSpec.describe "/groups", type: :request do
         get dashboard_path
       end
 
-      it 'does not delete the group and redirects to root_path' do
+      it 'does not delete the group and redirects to the last requested path (dashboard_path)' do
         expect do
           delete group_url(group)
         end.not_to change(Group, :count)
@@ -74,109 +182,4 @@ RSpec.describe "/groups", type: :request do
       end
     end
   end
-
-  # TODO: requestのテストを整理すること
-  # describe "GET /index" do
-  #   it "renders a successful response" do
-  #     Group.create! valid_attributes
-  #     get groups_url
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # describe "GET /show" do
-  #   it "renders a successful response" do
-  #     group = Group.create! valid_attributes
-  #     get group_url(group)
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # describe "GET /new" do
-  #   it "renders a successful response" do
-  #     get new_group_url
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # describe "GET /edit" do
-  #   it "renders a successful response" do
-  #     group = Group.create! valid_attributes
-  #     get edit_group_url(group)
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # describe "POST /create" do
-  #   context "with valid parameters" do
-  #     it "creates a new Group" do
-  #       expect {
-  #         post groups_url, params: { group: valid_attributes }
-  #       }.to change(Group, :count).by(1)
-  #     end
-
-  #     it "redirects to the created group" do
-  #       post groups_url, params: { group: valid_attributes }
-  #       expect(response).to redirect_to(group_url(Group.last))
-  #     end
-  #   end
-
-  #   context "with invalid parameters" do
-  #     it "does not create a new Group" do
-  #       expect {
-  #         post groups_url, params: { group: invalid_attributes }
-  #       }.not_to change(Group, :count)
-  #     end
-
-  #     it "renders a response with 422 status (i.e. to display the 'new' template)" do
-  #       post groups_url, params: { group: invalid_attributes }
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #     end
-  #   end
-  # end
-
-  # describe "PATCH /update" do
-  #   context "with valid parameters" do
-  #     let(:new_attributes) {
-  #       skip("Add a hash of attributes valid for your model")
-  #     }
-
-  #     it "updates the requested group" do
-  #       group = Group.create! valid_attributes
-  #       patch group_url(group), params: { group: new_attributes }
-  #       group.reload
-  #       skip("Add assertions for updated state")
-  #     end
-
-  #     it "redirects to the group" do
-  #       group = Group.create! valid_attributes
-  #       patch group_url(group), params: { group: new_attributes }
-  #       group.reload
-  #       expect(response).to redirect_to(group_url(group))
-  #     end
-  #   end
-
-  #   context "with invalid parameters" do
-  #     it "renders a response with 422 status (i.e. to display the 'edit' template)" do
-  #       group = Group.create! valid_attributes
-  #       patch group_url(group), params: { group: invalid_attributes }
-  #       expect(response).to have_http_status(:unprocessable_entity)
-  #     end
-  #   end
-  # end
-
-  # describe "DELETE /destroy" do
-  #   it "destroys the requested group" do
-  #     group = Group.create! valid_attributes
-  #     expect {
-  #       delete group_url(group)
-  #     }.to change(Group, :count).by(-1)
-  #   end
-
-  #   it "redirects to the groups list" do
-  #     group = Group.create! valid_attributes
-  #     delete group_url(group)
-  #     expect(response).to redirect_to(groups_url)
-  #   end
-  # end
 end
