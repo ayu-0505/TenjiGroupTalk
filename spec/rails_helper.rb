cabd98bd974550aaee891bcdc1d9efeaa4147e98
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-08-08 17:06:44 +0900

    JS用のドライバーをインストール

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,4 +74,11 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include LoginSupport
+  config.before(:each, type: :system) do |example|
+    if example.metadata[:js]
+      driven_by :selenium_chrome_headless, screen_size: [ 1400, 1400 ]
+    else
+      driven_by :rack_test
+    end
+  end
 end
