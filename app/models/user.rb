22374c5af5b2e3bb05f97833160b6d216e51338d
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-09-17 16:29:00 +0900

    sessionコントローにて、tokenの有無や有効性・ユーザー状態による条件分岐を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,23 +11,22 @@ class User < ApplicationRecord
     groups.include?(group)
   end
 
-  class << self
-    def find_or_create_from_auth_hash!(auth_hash)
-      user_params = user_params_from_auth_hash(auth_hash)
-      find_or_create_by!(uid: user_params[:uid]) do |user|
-        user.update(user_params)
-      end
+  def self.find_or_initialize_from_auth_hash!(auth_hash)
+    user_params = user_params_from_auth_hash(auth_hash)
+    find_or_initialize_by(uid: user_params[:uid]) do |user|
+      user.assign_attributes(user_params)
+      user.nickname = user_params[:name]
     end
+  end
 
-    private
+  private
 
-    def user_params_from_auth_hash(auth_hash)
-      {
-        name: auth_hash.info.name,
-        email: auth_hash.info.email,
-        uid: auth_hash.uid,
-        image: auth_hash.info.image
-      }
-    end
+  def self.user_params_from_auth_hash(auth_hash)
+    {
+      name: auth_hash.info.name,
+      email: auth_hash.info.email,
+      uid: auth_hash.uid,
+      image: auth_hash.info.image
+    }
   end
 end
