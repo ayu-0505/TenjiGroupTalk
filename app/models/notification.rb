198f0ad68e220cea1f2876ec5ddad3b5f3677123
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-09-20 23:16:11 +0900

    ダッシュボードに通知が表示されるようにした

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notifiable, polymorphic: true
 
+  scope :unread, -> { where(read: false) }
+
   def self.create_notifications_by(commenter, comment)
     subscribers = comment.talk.subscribers - [ commenter ]
 
@@ -11,6 +13,15 @@ class Notification < ApplicationRecord
         notifiable_type: 'Comment',
         notifiable_id: comment.id
       )
+    end
+  end
+
+  def link_title
+    case notifiable
+    when Comment
+      "#{notifiable.talk.title}に#{notifiable.user.name}よりコメントがありました"
+    else
+      '新しい通知があります'
     end
   end
 end
