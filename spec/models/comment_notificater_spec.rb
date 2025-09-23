d0826ad4e1ee58e87e61a425c33eaccde964b528
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-09-23 14:33:05 +0900

    購読通知に伴うmodel specを追加

---
+++ spec/models/comment_notificater_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe CommentNotificater, type: :model do
+  let(:user) { create(:user) }
+  let(:subscriber1) { create(:user) }
+  let(:subscriber2) { create(:user) }
+  let(:talk) { create(:talk) }
+  let(:comment) { create(:comment, user:, talk:) }
+
+  it 'creates notifications for the subscribers' do
+    talk.subscribers << subscriber1
+    talk.subscribers << subscriber2
+
+    expect {
+      ActiveSupport::Notifications.instrument('comment.create', user:, talk:, comment:)
+    }.to change(Notification, :count).by(2)
+    expect(subscriber1.notifications.last.notifiable).to eq comment
+    expect(subscriber2.notifications.last.notifiable).to eq comment
+  end
+
+  it 'dose not create a notification for the creator' do
+    talk.subscribers << user
+
+    expect {
+      ActiveSupport::Notifications.instrument('comment.create', user:, talk:, comment:)
+    }.not_to change(Notification, :count)
+  end
+end
---
+++ spec/models/talk_subscriber_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe TalkSubscriber, type: :model do
+  let(:user) { create(:user) }
+  let(:talk) { create(:talk) }
+
+  it 'creates a subscription for the talk creator' do
+    expect {
+      ActiveSupport::Notifications.instrument('talk.create', user: user, talk: talk)
+    }.to change(Subscription, :count).by(1)
+
+    subscription = Subscription.last
+    expect(subscription.user).to eq user
+    expect(subscription.talk).to eq talk
+  end
+end
