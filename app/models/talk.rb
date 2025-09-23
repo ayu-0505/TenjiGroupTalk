bc975778239971cb77957be781da425a29934c91
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-09-23 10:23:12 +0900

    必要なアソシエーションを追加

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,7 +2,8 @@ class Talk < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :comments
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
+  has_many :subscribers, through: :subscriptions, source: :user
   has_one :braille, as: :brailleable, dependent: :destroy
   accepts_nested_attributes_for :braille, allow_destroy: true
 
