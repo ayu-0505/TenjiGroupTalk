006fdc00004d3742f5350d1d769dd1cea75f41c9
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-09-23 15:12:13 +0900

    dashboradにおけるNotificationの表示テストを追加

---
+++ spec/system/dashboard_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Dashboard', type: :system do
+  describe 'notifications' do
+    it 'shows unread notifications and removes them after clicking' do
+      user = create(:user)
+      talk = create(:talk)
+      user.groups << talk.group
+      comment = create(:comment, talk:)
+      create(:comment_notification, user:, notifiable: comment)
+      log_in_as user
+
+      expect(page).to have_css('.notification')
+      click_on "#{talk.title}に#{comment.user.name}よりコメントがありました"
+      expect(page).to have_content(talk.description)
+      click_on 'トップへ戻る'
+      expect(page).to have_no_content("#{talk.title}に#{comment.user.name}よりコメントがありました")
+    end
+  end
+end
