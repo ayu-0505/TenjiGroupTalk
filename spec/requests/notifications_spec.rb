f354ff0e86918e853a1fb5691130af246994f4e5
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-10-07 14:41:53 +0900

    request testを追加

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Notifications', type: :request do
+  let(:user) { create(:user) }
+  let(:talk) { create(:talk) }
+
+  before do
+    user.groups << talk.group
+    sign_in(user)
+  end
+
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      get group_talks_url(talk.group)
+      expect(response).to be_successful
+    end
+  end
+
   describe 'PATCH /update' do
     it 'updates read column and redirect to notifiable link' do
-      user = create(:user)
-      talk = create(:talk)
-      user.groups << talk.group
       comment = create(:comment, talk:)
       notification = create(:comment_notification, user:, notifiable: comment)
-      sign_in(user)
-
       expect(notification.read).to be false
       patch notification_path(notification)
       notification.reload
