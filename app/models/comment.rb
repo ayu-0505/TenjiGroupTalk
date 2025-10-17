39802e55037970eacd128d35376455bcbd8f7b6e
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-10-17 17:00:07 +0900

    アソシエーションを新たに追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :talk
   belongs_to :braille, optional: true
-  has_many :notifications, as: :notifiable, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   validates :description, presence: true
 end
