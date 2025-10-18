1dc9c4ce126ce9fc1d4027b528d3e65615e439d0
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-10-18 11:27:04 +0900

    ページネーションを追加（適数などはデザイン時に確認する）

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @groups = current_user.groups.preload(:talks)
-    @unread_notifications = current_user.notifications.unread
+    @groups = current_user.groups.order(created_at: :desc)
+    @unread_notifications = current_user.notifications.unread.order(created_at: :desc)
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   before_action :authorize_group_member, only: %i[ show edit update destroy ]
 
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.order(created_at: :desc).page(params[:page])
   end
 
   def show
