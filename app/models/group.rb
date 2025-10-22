0616a7c27a80f339901b09d98342976323d55de4
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-10-22 15:00:22 +0900

    つけ忘れていたdependent: :destroyを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_many :talks
+  has_many :talks, dependent: :destroy
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
   has_many :invitations, dependent: :destroy
