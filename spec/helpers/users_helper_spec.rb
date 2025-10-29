28e6f01b55229d38d3c45e1b1a25890a72f64883
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-10-29 14:43:27 +0900

    #display_name()のテストを追加、user_name_tag()のテストを変更

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe UsersHelper, type: :helper do
 let(:user) { create(:user) }
 
-  describe '#display_user_name' do
+  describe '#user_name_tag' do
     it 'returns a dimmed nickname for a soft-deleted user' do
       user.soft_delete!
-      expect(display_user_name(user)).to have_css('span.text-gray-400', text: "#{user.nickname}")
+      expect(user_name_tag(user)).to have_css('span.text-gray-400', text: "#{user.nickname}")
     end
 
-    it 'returns a normal nickname for an active user' do
-      expect(display_user_name(user)).to eq user.nickname
+    it 'returns a nomal display name for an active user' do
+      expect(user_name_tag(user)).to eq (user.nickname || usr.name)
     end
   end
 end
