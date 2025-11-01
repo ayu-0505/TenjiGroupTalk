30d58da407d743caeda46624cc0dd602d1cfec24
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-11-01 11:44:10 +0900

    Commentのupdate順（またはTalkのupdate順）のスコープを作成

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -8,4 +8,12 @@ class Talk < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true
+
+  scope :sort_by_latest_comments, -> {
+    left_joins(:comments)
+    .group('talks.id')
+    .select('talks.*, MAX(COALESCE(comments.updated_at, talks.updated_at)) AS comments_updated_at')
+    .reorder(Arel.sql('comments_updated_at DESC'))
+    .order('comments_updated_at DESC')
+  }
 end
