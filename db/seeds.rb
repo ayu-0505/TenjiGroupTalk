a28425f0aec8054726c4ba0ebd1b41ecae335566
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-11-08 14:03:27 +0900

    SEEDデータが冪等性を持つように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,73 @@
 # frozen_string_literal: true
 
+return unless Rails.env.development?
+
+Subscription.destroy_all
+Notification.destroy_all
+Invitation.destroy_all
+Membership.destroy_all
+Comment.destroy_all
+Talk.destroy_all
+Braille.destroy_all
+Group.destroy_all
+Talk.destroy_all
+User.destroy_all
+
 ORIGINAL_TEXTS = %w[てんじ いるか ことば こんにちわ ゆめ あさがお morning 100 ]
 
-first_admin = User.find_or_create_by!(uid: 'first_admin') do |user|
-  user.name = '点訳部つくる'
-  user.email = 'user1@example.com'
-  user.image = ActionController::Base.helpers.asset_path('test_user_icon.png')
-end
+first_admin = User.create!(
+  uid: 'first_admin',
+  name: '点訳部つくる',
+  email: 'user1@example.com',
+  image: (ActionController::Base.helpers.asset_path 'test_user_icon.png'))
 
-second_admin = User.find_or_create_by!(uid: 'second_admin') do |user|
-  user.name = '兼任点字部長'
-  user.email = 'user2@example.com'
-  user.image = ActionController::Base.helpers.asset_path('test_user_icon.png')
-end
+second_admin = User.create!(
+  uid: 'second_admin',
+  name: '兼任点字部長',
+  email: 'user2@example.com',
+  image: (ActionController::Base.helpers.asset_path 'test_user_icon.png'))
 
-User.find_or_create_by!(uid: 'user_without_group') do |user|
-  user.name = 'グループ未加入子'
-  user.email = 'user3@example.com'
-  user.image = ActionController::Base.helpers.asset_path('test_user_icon.png')
-end
+User.create!(
+  uid: 'user_without_group',
+  name: 'グループ未加入子',
+  email: 'user3@example.com',
+  image: (ActionController::Base.helpers.asset_path 'test_user_icon.png'))
 
-user_with_multiple_groups = User.find_or_create_by!(uid: 'user_with_multiple_groups') do |user|
-  user.name = 'グループ複数加入子'
-  user.email = 'user4@example.com'
-  user.image = ActionController::Base.helpers.asset_path('test_user_icon.png')
-end
 
-User.find_or_create_by!(uid: 'deleted_user') do |user|
-  user.nickname = 'サービスやめお'
-  user.name = 'deleted_name'
-  user.email = 'dummy_email_0000',
-  user.uid = 'dummy_uid_0000'
-  user.deleted_at = Time.current
-end
+user_with_multiple_groups = User.create!(
+  uid: 'user_with_multiple_groups',
+  name: 'グループ複数加入子',
+  email: 'user4@example.com',
+  image: (ActionController::Base.helpers.asset_path 'test_user_icon.png'))
+
+
+User.create!(
+  uid: 'deleted_user',
+  nickname: 'サービスやめお',
+  name: 'deleted_name',
+  email: 'dummy_email_0000',
+  image: (ActionController::Base.helpers.asset_path 'test_user_icon.png'),
+  deleted_at: Time.current)
 
 users = []
 40.times do |n|
-  user = User.find_or_create_by!(uid: "uid_user_0#{n}") do |u|
-           u.name = "ユーザー#{n}"
-           u.email = "other-user#{n}@example.com",
-           u.uid = "uid_other_user_#{n}",
-           u.nickname = "ニックネーム#{n}",
-           u.image = ActionController::Base.helpers.asset_path('test_user_icon.png')
-         end
+  user = User.create!(
+          uid: "uid_user_0#{n}",
+          name: "ユーザー#{n}",
+          email: "other-user#{n}@example.com",
+          nickname: "ニックネーム#{n}",
+          image: (ActionController::Base.helpers.asset_path 'test_user_icon.png'))
   users << user
 end
 
 3.times do |n|
-  unless Group.exists?(name: "First Admin Group #{n}")
-    group = Group.create!({
-      name: "First Admin Group #{n}",
-      admin_id: first_admin.id
-    })
-    first_admin.groups << group
-    user_with_multiple_groups.groups << group
-  end
+  group = Group.create!({
+    name: "First Admin Group #{n}",
+    admin_id: first_admin.id
+  })
+
+  first_admin.groups << group
+  user_with_multiple_groups.groups << group
 end
 
 first_group = Group.find_by(name: 'First Admin Group 0')
@@ -62,65 +75,57 @@ second_admin.groups << first_group
 users.each { |user| user.groups << first_group }
 
 3.times do |n|
-  unless Group.exists?(name: "Second Admin Group #{n}")
-    group = Group.create!({
-      name: "Second Admin Group #{n}",
-      admin_id: second_admin.id
-    })
-    second_admin.groups << group
-    user_with_multiple_groups.groups << group
-  end
+  group = Group.create!({
+    name: "Second Admin Group #{n}",
+    admin_id: second_admin.id
+  })
+  second_admin.groups << group
+  user_with_multiple_groups.groups << group
 end
 
 10.times do |n|
-  unless Talk.exists?(title: "Talk Title #{n}")
-    talk = Talk.create!({
-      title: "Talk Title #{n}",
-      description: "今日も楽しく点字を勉強しましょう。このトークは#{n}番目です。",
-      user: first_admin,
-      group: first_group
-    })
+  talk = Talk.create!({
+    title: "Talk Title #{n}",
+    description: "今日も楽しく点字を勉強しましょう。このトークは#{n}番目です。",
+    user: first_admin,
+    group: first_group
+  })
+  Subscription.create!({
+    user: first_admin,
+    talk:
+  })
+end
+
+10.times do |n|
+  braille = Braille.create(original_text: ORIGINAL_TEXTS.sample)
+  talk = Talk.create!({
+    title: "トークテーマ #{n}",
+    description: "私もトークを作ってみました。このトークは#{n}番目です。",
+    user: user_with_multiple_groups,
+    group: first_group,
+    braille:
+  })
     Subscription.create!({
-      user: first_admin,
-      talk:
-    })
-  end
-end
-
-10.times do |n|
-  unless Talk.exists?(title: "トークテーマ #{n}")
-    braille = Braille.create(original_text: ORIGINAL_TEXTS.sample)
-    talk = Talk.create!({
-      title: "トークテーマ #{n}",
-      description: "私もトークを作ってみました。このトークは#{n}番目です。",
-      user: user_with_multiple_groups,
-      group: first_group,
-      braille:
-    })
-      Subscription.create!({
-      user: first_admin,
-      talk:
-    })
-  end
+    user: first_admin,
+    talk:
+  })
 end
 
 20.times do |n|
-  unless Comment.exists?(description: "これはコメントです コメントナンバーは#{n}です")
-    user = user_with_multiple_groups
-    talk = first_group.talks.first
-    braille = Braille.create!({
-      original_text: ORIGINAL_TEXTS.sample,
-      user:
-    })
-    comment = Comment.create!({
-      description: "これはコメントです コメントナンバーは#{n}です",
-      user:,
-      talk:,
-      braille:
-    })
-    Notification.create!({
-      user: talk.user,
-      comment:
-    })
-  end
+  user = user_with_multiple_groups
+  talk = first_group.talks.first
+  braille = Braille.create!({
+    original_text: ORIGINAL_TEXTS.sample,
+    user:
+  })
+  comment = Comment.create!({
+    description: "これはコメントです コメントナンバーは#{n}です",
+    user:,
+    talk:,
+    braille:
+  })
+  Notification.create!({
+    user: talk.user,
+    comment:
+  })
 end
