63d641045fc736c4fdeb5abdd21f3cc84d078eb9
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-12-04 14:34:42 +0900

    groupを選択できるようにし、session中は保持できるように変更

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class DashboardController < ApplicationController
+  before_action :set_selected_group, only: :index
+
   def index
-    @group = current_user.groups.order(created_at: :desc).first
+    @groups = current_user.groups.order(created_at: :desc)
+    @group = @selected_group || @groups.first
   end
+
+  private
+    def set_selected_group
+      group_id = params[:group_id] || session[:dashboard_group_id]
+      @selected_group = current_user.groups.find_by(id: group_id)
+      session[:dashboard_group_id] = @selected_group&.id
+    end
 end
