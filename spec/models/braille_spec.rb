307a1e2f347c993242063fbf6935bd6086a15dcb
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-12-10 15:20:35 +0900

    brailleモデルのテストを追加

--- spec/models/braille_spec.rb
+++ spec/models/braille_spec.rb
@@ -4,6 +4,15 @@ RSpec.describe Braille, type: :model do
   let(:user) { create(:user) }
   let(:braille) { create(:braille, user:) }
 
+  describe 'when creating a new braille without original_text' do
+    it "sets raised_braille to nil when original_text is blank" do
+      braille = described_class.new(original_text: '')
+      braille.send(:initialize_braille)
+      expect(braille.raised_braille).to be_nil
+      expect(braille.indented_braille).to be_nil
+    end
+  end
+
   # WARNING: 点字のスペースはスペースのコードではなく、点字の無点（U+2800）
   describe 'same_content?' do
     context 'when same text' do
