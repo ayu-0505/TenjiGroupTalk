be0f4dde015f68bf5cbe8c8fdaeedb3d71ec8c62
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-12-22 12:42:04 +0900

    通信エラー時には500エラーを返すように変更。合わせて、JS側の例外処理を削除

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -4,26 +4,19 @@ class Api::SubscriptionsController < ApplicationController
     return unless current_user.groups.exists?(id: talk.group.id)
 
     subscription = current_user.subscriptions.build(talk:)
+    if subscription.save!
       flash.now[:notice] = '通知登録しました'
       html = render_to_string partial: 'layouts/flash'
       render json: { id: subscription.id, html: }, status: :ok
-    else
-      flash.now[:alert] = '通信中にエラーが発生しました'
-      html = render_to_string partial: 'layouts/flash'
-      render json: { html: }, status: :unprocessable_entity
     end
   end
 
   def destroy
     subscription = current_user.subscriptions.find(params[:id])
-    if subscription.destroy
+    if subscription.destroy!
       flash.now[:notice] = '通知登録を解除しました'
       html = render_to_string partial: 'layouts/flash'
       render json: { html: }, status: :ok
-    else
-      flash.now[:alert] = '通信中にエラーが発生しました'
-      html = render_to_string partial: 'layouts/flash'
-      render json: { html: }, status: :unprocessable_entity
     end
   end
 end
