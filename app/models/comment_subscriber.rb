c48050535bac2329a6b526e0ffd775b6c037539f
ayu-0505 <amehuri.boshi.sh@icloud.com>
2026-01-03 13:51:52 +0900

    ActiveSupport::Notifications使用部分を削除

--- app/models/comment_subscriber.rb
+++
@@ -1,7 +0,0 @@
-class CommentSubscriber
-  def call(_name, _started, _finished, _unique_id, payload)
-    user = payload[:user]
-    talk = payload[:talk]
-    Subscription.find_or_create_by!(user:, talk:)
-  end
-end
--- config/initializers/active_support_notifications.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.reloader.to_prepare do
-  ActiveSupport::Notifications.subscribe('talk.create', TalkSubscriber.new)
-  ActiveSupport::Notifications.subscribe('comment.create', CommentSubscriber.new)
-  ActiveSupport::Notifications.subscribe('comment.create', CommentNotificater.new)
-end
