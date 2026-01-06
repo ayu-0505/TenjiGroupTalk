dfa064ae97aa8e3d722d57cc5223c0e7ff3bb4a2
ayu-0505 <amehuri.boshi.sh@icloud.com>
2026-01-06 14:00:55 +0900

    不要なif文を削除

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -3,19 +3,17 @@ class Api::SubscriptionsController < ApplicationController
     talk = Talk.joins(:group).where(group: { id: current_user.group_ids }).find(params.expect(:talk_id))
 
     subscription = current_user.subscriptions.build(talk:)
-    if subscription.save!
-      flash.now[:notice] = '通知登録しました'
-      html = render_to_string partial: 'layouts/flash'
-      render json: { id: subscription.id, html: }, status: :ok
-    end
+    subscription.save!
+    flash.now[:notice] = '通知登録しました'
+    html = render_to_string partial: 'layouts/flash'
+    render json: { id: subscription.id, html: }, status: :ok
   end
 
   def destroy
     subscription = current_user.subscriptions.find(params[:id])
-    if subscription.destroy!
-      flash.now[:notice] = '通知登録を解除しました'
-      html = render_to_string partial: 'layouts/flash'
-      render json: { html: }, status: :ok
-    end
+    subscription.destroy!
+    flash.now[:notice] = '通知登録を解除しました'
+    html = render_to_string partial: 'layouts/flash'
+    render json: { html: }, status: :ok
   end
 end
